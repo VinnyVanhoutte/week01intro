a014fe3c7dc20c98ffca34a30940ab9bce3096bf
VinnyVanhoutte <vinny.vanhoutte@student.howest.be>
2017-09-26 09:28:53 +0200

    week 01 intro

---
+++ oef 01.py
@@ -0,0 +1,8 @@
+# Dit is commentaar (commentarieer je programma!)
+#  intro oefeningen
+
+
+x= int(input("geef een getal."))
+y= int(input("geef nog een getal."))
+kwadraat= (x + y) * (x + y)
+print(("( {0} + {1} )² = {2}").format(x,y,kwadraat))
